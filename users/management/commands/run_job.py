808522a27296ee4c59457b7a6336d2b449bf3a78
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2024-10-13 06:46:50 +0500

    A

--- users/management/commands/run_job.py
+++ users/management/commands/run_job.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+import datetime
 from django.utils import timezone
 from django.db.models import Q
 from users.models import Report
@@ -18,7 +19,7 @@ class Command(BaseCommand):
             one_day_ago = today - timezone.timedelta(days=1)
             two_days_ago = today - timezone.timedelta(days=2)
             reports = Report.objects.all()
-
+            self.log()
             # Process each report
             for report in reports:
                 print(f"PROCESSING {report}")
@@ -26,6 +27,21 @@ class Command(BaseCommand):
                                one_day_ago, report.ad_unit_ids.split(","), report.cpm_rate, report.name)
                 report.end_date = one_day_ago
                 report.save()
+                log_file_path = '~/job_log.txt'
+                current_time = datetime.datetime.now()
+                timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+                with open(log_file_path, 'a') as file:
+                    file.write(f"Job finished for {one_day_ago} for report {report.pk} {report.name}: {timestamp}\n")
                 print(f"Processed Report: {report}")
         except Exception as e:
             print(f"JOB FAILED - Error: {e}")
+
+    def log():
+        current_time = datetime.datetime.now()
+
+        timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        log_file_path = '~/job_log.txt'
+        with open(log_file_path, 'a') as file:
+            file.write(f"Job ran on: {timestamp}\n")
+
+        print(f"Timestamp added to {log_file_path}")
