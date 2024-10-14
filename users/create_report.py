59f9580a1f139be53dc4068f5be4856b7b65077f
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2024-10-15 03:15:43 +0500

    A

--- users/create_report.py
+++ users/create_report.py
@@ -44,10 +44,10 @@ def fetch_ad_units():
     return results
 
 
-def process_report(id, start_date, end_date, ad_unit_ids, cpm_rate, name=None):
+def process_report(id, start_date, end_date, ad_unit_ids, cpm_rate):
     client = ad_manager.AdManagerClient.LoadFromStorage("~/googleads.yaml")
     today = timezone.now().date()
-    print(f"R {ad_unit_ids} report id {id} report name {name}")
+
 # Check if the given date is equal to or greater than today
     end_date = today - timedelta(days=1)
     report_job = {
@@ -76,7 +76,6 @@ def process_report(id, start_date, end_date, ad_unit_ids, cpm_rate, name=None):
             }
         }
     }
-    print(f"REPORT QUERY {report_job['reportQuery']}")
     if len(ad_unit_ids) > 0:
         # Build the WHERE statement using ad_unit_ids
         statement = (ad_manager.StatementBuilder(version='v202408')
@@ -84,52 +83,48 @@ def process_report(id, start_date, end_date, ad_unit_ids, cpm_rate, name=None):
                      .Limit(None)
                      .Offset(None))
         report_job["reportQuery"]["statement"] = statement.ToStatement()
-    print(f"report_job {report_job["reportQuery"]["statement"]["query"]}")
+
+    # Initialize the DataDownloader and run the report
     report_downloader = client.GetDataDownloader(version='v202408')
     report_job_id = report_downloader.WaitForReport(report_job)
 
-    with open("/root/report.csv.gz", 'wb') as report_file:
+    # Temporary file for storing the downloaded report
+
+    with tempfile.NamedTemporaryFile(suffix='.csv.gz', mode='wb', delete=False) as report_file:
         report_downloader.DownloadReportToFile(
             report_job_id, 'CSV_DUMP', report_file)
-        print(f"FILE IS {report_file.name}")
-        try:
-            # Process the CSV file and save each record
-            report = Report.objects.get(id=id)
-            with gzip.open(report_file.name, 'rt') as f:
-                file_content = f.read()
-                print(file_content)
-            df = pd.read_csv(report_file.name, compression='gzip')
-            
-            for _index, row in df.iterrows():
-                # Headers are used by default
+    try:
+        # Process the CSV file and save each record
+        report = Report.objects.get(id=id)
+        df = pd.read_csv(report_file.name, compression='gzip')
+        for _index, row in df.iterrows():
+            # Headers are used by default
 
-                # Assuming columns: Date, AdUnitID, AdUnitName, Impressions, Clicks, CTR
-                # Adjust index if CSV structure changes
-                date = datetime.strptime(row[0], '%Y-%m-%d').date()
-                ad_unit_id = str(row[1])
-                ad_unit_name = row[2]
-                impressions = int(row[3])
-                clicks = str(row[4])
-                ctr = float(row[5])
-                # Calculate revenue using the formula (cpm_rate * impressions) / 1000
-                revenue = (cpm_rate * impressions) / 1000
-                # Save the record in the database
-                Record.objects.update_or_create(
-                    report=report,
-                    date=date,
-                    ad_unit_id=ad_unit_id,
-                    ad_unit_name=ad_unit_name,
-                    defaults={
-                        'impressions': str(impressions),
-                        'clicks': clicks,
-                        'ctr': ctr,
-                        'revenue': str(revenue),
-                    }
-                )
-            report.status = 'Done'
-            print(f"REPORT {report}")
-            report.save()
-        finally:
-            print(f"{report_file.name}")
-            # if (os.path.exists(report_file.name)):
-            #     os.remove(report_file.name)
+            # Assuming columns: Date, AdUnitID, AdUnitName, Impressions, Clicks, CTR
+            # Adjust index if CSV structure changes
+            date = datetime.strptime(row[0], '%Y-%m-%d').date()
+            ad_unit_id = str(row[1])
+            ad_unit_name = row[2]
+            impressions = int(row[3])
+            clicks = str(row[4])
+            ctr = float(row[5])
+            # Calculate revenue using the formula (cpm_rate * impressions) / 1000
+            revenue = (cpm_rate * impressions) / 1000
+            # Save the record in the database
+            Record.objects.update_or_create(
+                report=report,
+                date=date,
+                ad_unit_id=ad_unit_id,
+                ad_unit_name=ad_unit_name,
+                defaults={
+                    'impressions': str(impressions),
+                    'clicks': clicks,
+                    'ctr': ctr,
+                    'revenue': str(revenue),
+                }
+            )
+        report.status = 'Done'
+        report.save()
+    finally:
+        if (os.path.exists(report_file.name)):
+            os.remove(report_file.name)
